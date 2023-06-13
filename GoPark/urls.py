df3517872e01900cbbb07c72245019b01d9497eb
dominikdziuba <dominikdziuba05@gmail.com>
2023-06-13 05:13:13 +0200

    Final routing

--- GoPark/urls.py
+++ GoPark/urls.py
@@ -6,7 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', include('base.urls')),
-    path('', include('frontend.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
  
--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-#    path('cars/', CarsView.as_view(), name="cars"),
- #   path('addcar/', AddCarView.as_view(), name="addcar"),
-  #  path('payments/', PaymentsView.as_view(), name="payments"),
-   # path('history/', name='history'),
+    path('cars/', CarsView.as_view(), name="cars"),
+    path('payments/', PaymentsView.as_view(), name="payments"),
+
 ]
